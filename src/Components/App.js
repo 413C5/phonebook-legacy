74655f03e67b7f291d9f65f7da1079361a7d3393
413C5 <a_gonzalezzarate2@yahoo.com>
2023-02-22 22:01:41 -0600

    Paso 2 ejercicio2.7
    Se emplea alert para mandar el mensaje de que el nombre ya existe

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,34 +3,38 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
-  const [newName, setNewName] = useState('')
-  const handleNewNameChange = event => setNewName(event.target.value)
-  const handleAddNewPerson = event => {
-    event.preventDefault()
-    const newPerson = { name: newName }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-  }
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={handleAddNewPerson}>
+    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+    const [newName, setNewName] = useState('')
+    const handleNewNameChange = event => setNewName(event.target.value)
+    const handleAddNewPerson = event => {
+        event.preventDefault()
+        if (persons.find(x => x.name === newName)) {
+            alert(`${newName} is already added to phonebook`)
+        } else {
+            const newPerson = { name: newName }
+            setPersons(persons.concat(newPerson))
+            setNewName('')
+        }
+    }
+    return (
         <div>
-          name: <input value={newName} onChange={handleNewNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map(x => (
-        /*<div>debug{newName}</div>,*/
-        <div key={x.name}>{x.name}</div>/* ,
+            <h2>Phonebook</h2>
+            <form onSubmit={handleAddNewPerson}>
+                <div>
+                    name: <input value={newName} onChange={handleNewNameChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(x => (
+                /*<div>debug{newName}</div>,*/
+                <div key={x.name}>{x.name}</div>/* ,
         <div>debug{newName}</div> */
-      ))}
-    </div>
-  )
+            ))}
+        </div>
+    )
 }
 
 export default App
