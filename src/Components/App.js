173750a2a18b949e8e29aa6d272e9fce9350a77f
413C5 <a_gonzalezzarate2@yahoo.com>
2023-02-22 22:46:21 -0600

    Paso 3 ejercicio 2.8
    Se agrego la opcion de colocar número de teléfono

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,17 +3,20 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+    const [persons, setPersons] = useState([{ name: 'Arto Hellas' ,number:'040-1234567'}])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
     const handleNewNameChange = event => setNewName(event.target.value)
+    const handleNewNumberChange = event => setNewNumber(event.target.value)
     const handleAddNewPerson = event => {
         event.preventDefault()
         if (persons.find(x => x.name === newName)) {
             alert(`${newName} is already added to phonebook`)
         } else {
-            const newPerson = { name: newName }
+            const newPerson = { name: newName ,number:newNumber}
             setPersons(persons.concat(newPerson))
             setNewName('')
+            setNewNumber('')
         }
     }
     return (
@@ -24,13 +27,16 @@ const App = () => {
                     name: <input value={newName} onChange={handleNewNameChange} />
                 </div>
                 <div>
+                    number: <input value={newNumber} onChange={handleNewNumberChange} />
+                </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             {persons.map(x => (
                 /*<div>debug{newName}</div>,*/
-                <div key={x.name}>{x.name}</div>/* ,
+                <div key={x.name}>{x.name} {x.number}</div>/* ,
         <div>debug{newName}</div> */
             ))}
         </div>
