d6a827910825a52160c6d89b5a8a04b9104b112b
413C5 <a_gonzalezzarate2@yahoo.com>
2023-02-26 14:48:01 -0600

    Paso 4 Ejercicio 2.9
    Se implemento un campo de búsqueda en donde se filtra la lista de personas por nombre

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
-
-import { useState } from 'react'
+import React ,{ useState } from 'react'
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: 'Arto Hellas' ,number:'040-1234567'}])
-    const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+      ])
+    const [newName, setNewName] = useState('')  
     const [newNumber, setNewNumber] = useState('')
+    const [filter,setFilter]=useState('')
     const handleNewNameChange = event => setNewName(event.target.value)
     const handleNewNumberChange = event => setNewNumber(event.target.value)
+    const handleSetFilter = event => setFilter(event.target.value)
     const handleAddNewPerson = event => {
         event.preventDefault()
         if (persons.find(x => x.name === newName)) {
@@ -22,6 +27,9 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <p>
+                filther shown with <input value={filter} onChange={handleSetFilter} />
+            </p>
             <form onSubmit={handleAddNewPerson}>
                 <div>
                     name: <input value={newName} onChange={handleNewNameChange} />
@@ -34,7 +42,8 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(x => (
+            {persons.filter(x=>x.name.toLowerCase().includes(filter.toLowerCase()))
+            .map(x => (
                 /*<div>debug{newName}</div>,*/
                 <div key={x.name}>{x.name} {x.number}</div>/* ,
         <div>debug{newName}</div> */
