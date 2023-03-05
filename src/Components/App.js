3f2832b668e938585e790545a6cd307b9311eb8b
413C5 <a_gonzalezzarate2@yahoo.com>
2023-03-04 22:00:59 -0600

    Paso 6 Ejercicio 2.11
    Se almacena el edo. inicial de la aplicación en un .json y se hace uso de hooks con axios

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForms'
 import Persons from './Persons'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
 
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
@@ -29,6 +25,16 @@ const App = () => {
             setNewNumber('')
         }
     }
+
+    useEffect(()=>{
+        console.log('Entro al Effect');
+        axios
+          .get('http://localhost:3001/persons')
+          .then(response=>{
+            console.log('Entro al then');
+            setPersons(response.data)
+          })
+      },[])
 
     return (
         <div>
