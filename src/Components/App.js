1a1d19213279cc769d1deeb20f1d65c467a12ba4
413C5 <a_gonzalezzarate2@yahoo.com>
2023-03-21 15:08:22 -0600

    Ejercicio 2.18 paso 10
    Se permite la actualizacion del numero de existentes y se actualizo el delete

--- src/Components/App.js
+++ src/Components/App.js
@@ -24,7 +24,29 @@ const App = () => {
   const addName = (event) => {
     event.preventDefault()
     if (persons.find((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+
+      //Se busca que exista la persona con ese nombre
+      const person = persons.find((person) => person.name === newName)
+
+      //Se copia el arreglo y se copia todo, pero se define el nuevo numero
+      const person2 = { ...person, number: newNumber}
+
+      console.log(person.id)
+      console.log(person)
+      console.log(person2)
+
+      if(window.confirm(`${newName} is already added to phonebook,replace old number with a new one?`)){
+        personService
+          .update(person.id,person2)
+          .then(response=>{
+             setPersons(persons.map(x=>{
+              if(x.id!==person.id)
+                return x
+              else
+                return response
+            })) 
+          })
+      }
     }
     else {
       const nameObject = {
@@ -63,16 +85,34 @@ const App = () => {
   }
 
   const deletePerson = (id) => {
+    //Objeto a eliminar
     const person = persons.find(x => x.id === id)
+    
+    //Creacion de nuevo arreglo sin el objeto
+    const person2 = persons.filter(x => x.id !== person.id );
+      
 
-    console.log(person.name)
+    /* console.log(person)
+    console.log(person2) */
     if (window.confirm(`Delete ${person.name} ?`)) {
       personService
         .remove(id)
-        .then(response => {
-          console.log(`Eliminado ${person.name} `)
-        }
-        )
+        .then(response => {          
+          //Se actualiza el arreglo mostrado en pantalla
+          setPersons(person2)
+
+          /* setPersons(x=>{
+            if(x.id!==id)
+              return person
+            else
+              return response
+          }) */
+
+          /* console.log('hola')
+          console.log(response) */
+
+          //console.log(response)
+        })
     }
   }
 
