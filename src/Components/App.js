5183f89da3674b917524c220e0eaaf6fbf4ad790
413C5 <a_gonzalezzarate2@yahoo.com>
2023-03-21 16:50:43 -0600

    Ejercicio 2.19 Paso 11
    Se muestra una notificación de unos pocos segundos despues de realizar una operación exitosa

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Filter from "./Filter";
-import axios from 'axios';
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
 import personService from "../Services/persons"
+import Notification from "./Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  const [state,setState]=useState(false)
 
   //Recuperación de datos
   useEffect(() => {
@@ -29,25 +31,49 @@ const App = () => {
       const person = persons.find((person) => person.name === newName)
 
       //Se copia el arreglo y se copia todo, pero se define el nuevo numero
-      const person2 = { ...person, number: newNumber}
+      const person2 = { ...person, number: newNumber }
 
-      console.log(person.id)
-      console.log(person)
-      console.log(person2)
+      /*console.log(person.id)
+       console.log(person)
+       console.log(person2) */
 
-      if(window.confirm(`${newName} is already added to phonebook,replace old number with a new one?`)){
+      //Actualización de número
+      if (window.confirm(`${newName} is already added to phonebook,replace old number with a new one?`)) {
         personService
-          .update(person.id,person2)
-          .then(response=>{
-             setPersons(persons.map(x=>{
-              if(x.id!==person.id)
+          .update(person.id, person2)
+          .then(response => {
+            setPersons(persons.map(x => {
+              //Si id no se encuentra
+              if (x.id !== person.id)
                 return x
+              //Si se encuentra id
               else
                 return response
-            })) 
+            }))
+
+            //Se muestra el mensaje por 5 seg
+            setMessage(`Updated ${newName} with number ${newNumber}`)
+            setState(true)
+            setTimeout(() => {
+              setMessage(null)
+              setState(false)
+            }, 5000);
+
           })
+
+        /* .catch(error=>{
+          setErrorMessage(`Information of '${person2.name}' has already been updated from server`)
+          .setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+          setPersons(persons.filter(x=>x.id!==person.id))
+        }) */
+
+        console.log(`Updated ${newName} with number ${newNumber}`)
       }
     }
+
+    //Agrega nueva persona
     else {
       const nameObject = {
         name: newName,
@@ -61,7 +87,16 @@ const App = () => {
           setPersons(persons.concat(response))
           setNewName('')
           setNewNumber('')
+          setMessage(`Added ${newName}`)
+          setState(true)
+          //Despues de 5 segundos desaparece el mensaje
+          setTimeout(() => {
+            setMessage(null)
+            setState(false)
+          }, 5000);
         })
+
+        console.log(`Added ${newName}`)
     }
   }
 
@@ -87,38 +122,45 @@ const App = () => {
   const deletePerson = (id) => {
     //Objeto a eliminar
     const person = persons.find(x => x.id === id)
-    
+
     //Creacion de nuevo arreglo sin el objeto
-    const person2 = persons.filter(x => x.id !== person.id );
-      
+    const person2 = persons.filter(x => x.id !== person.id);
+
 
     /* console.log(person)
     console.log(person2) */
     if (window.confirm(`Delete ${person.name} ?`)) {
       personService
         .remove(id)
-        .then(response => {          
+        .then(response => {
           //Se actualiza el arreglo mostrado en pantalla
           setPersons(person2)
 
-          /* setPersons(x=>{
-            if(x.id!==id)
-              return person
-            else
-              return response
-          }) */
-
-          /* console.log('hola')
-          console.log(response) */
-
-          //console.log(response)
+          //Desaparece el mensaje posterior a 5 seg
+          setMessage(`Deleted ${person.name} with an id of ${person.id} `)
+          setState(true)
+          setTimeout(() => {
+            setMessage(null)
+            setState(false)
+          }, 5000);
         })
+
+      /* .catch(error=>{
+        setErrorMessage(`Information of '${person2.name}' has already been removed from server`)
+        setTimeout(() => {
+          setErrorMessage(null)            
+        }, 5000)
+        setPersons(person.filter(x=>x.id!==id))
+      }) */
+
+      console.log(`Deleted ${person.name} with an id of ${person.id} `)
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} state={state} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h2>Add a new:</h2>
       <PersonForm
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App';
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
